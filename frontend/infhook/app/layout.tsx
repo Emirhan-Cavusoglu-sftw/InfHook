c58c347f9e1dff11e7e88b78c38fdb93278aa9b7
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-08-29 09:59:56 +0300

    Nextjs vercel configuration

--- frontend/infhook/app/layout.tsx
+++ frontend/infhook/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Provider>
           <HookProvider>
-            <Header />
+            <Header /> 
             <Suspense>
 
             {children}
